6febeb756c8e9c44e2f7a857b3d2311731968e70
Vinod <vinodk@bigrattle.com>
2019-07-15 12:55:56 +0530

    composer json and proc file added

--- index.php
+++ index.php
@@ -1,18 +1,5 @@
 <?php
 
-// $ch = curl_init();
-// curl_setopt($ch, CURLOPT_URL, "http://www.kimonolabs.com/api/e45oypq8?apikey=xxxx");
-// curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-// $parsed_json = curl_exec($ch);
-// $parsed_json = json_decode($parsed_json);
-
-// foreach($parsed_json->results->collection1 as $collection){
-//     echo $collection->title->text . '<br>';
-//     echo $collection->title->href . '<br>';
-//     echo $collection->posted . '<br><br>';
-// }
-
-// curl_close($ch);
 
 $method = $_SERVER['REQUEST_METHOD']
 if ($method = "POST"){
